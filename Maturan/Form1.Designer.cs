2d67d35bf760e35488cbe95140f05c95f85298a6
ACT-STUDENT <ACT-STUDENT@LAB6-PC15>
2025-07-22 18:17:12 +0800

    Add project files.

---
+++ Maturan/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace Maturan
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblTitle = new Label();
+            txtUser = new TextBox();
+            txtPass = new TextBox();
+            lblUser = new Label();
+            lblPass = new Label();
+            btn_Login = new Button();
+            SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            lblTitle.AutoSize = true;
+            lblTitle.BackColor = Color.Transparent;
+            lblTitle.Font = new Font("Showcard Gothic", 48F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblTitle.ForeColor = Color.White;
+            lblTitle.Location = new Point(232, 61);
+            lblTitle.Name = "lblTitle";
+            lblTitle.Size = new Size(240, 79);
+            lblTitle.TabIndex = 0;
+            lblTitle.Text = "LOG IN";
+            // 
+            // txtUser
+            // 
+            txtUser.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtUser.ForeColor = SystemColors.ScrollBar;
+            txtUser.Location = new Point(208, 194);
+            txtUser.Multiline = true;
+            txtUser.Name = "txtUser";
+            txtUser.Size = new Size(293, 32);
+            txtUser.TabIndex = 1;
+            txtUser.Text = "Username";
+            // 
+            // txtPass
+            // 
+            txtPass.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtPass.ForeColor = SystemColors.ScrollBar;
+            txtPass.Location = new Point(208, 287);
+            txtPass.Multiline = true;
+            txtPass.Name = "txtPass";
+            txtPass.Size = new Size(293, 32);
+            txtPass.TabIndex = 2;
+            txtPass.Text = "Password";
+            // 
+            // lblUser
+            // 
+            lblUser.AutoSize = true;
+            lblUser.BackColor = Color.Transparent;
+            lblUser.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblUser.ForeColor = Color.White;
+            lblUser.Location = new Point(208, 169);
+            lblUser.Name = "lblUser";
+            lblUser.Size = new Size(84, 21);
+            lblUser.TabIndex = 3;
+            lblUser.Text = "Username:";
+            // 
+            // lblPass
+            // 
+            lblPass.AutoSize = true;
+            lblPass.BackColor = Color.Transparent;
+            lblPass.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblPass.ForeColor = Color.White;
+            lblPass.Location = new Point(208, 260);
+            lblPass.Name = "lblPass";
+            lblPass.Size = new Size(79, 21);
+            lblPass.TabIndex = 4;
+            lblPass.Text = "Password:";
+            // 
+            // btn_Login
+            // 
+            btn_Login.BackColor = Color.Transparent;
+            btn_Login.FlatStyle = FlatStyle.Flat;
+            btn_Login.Font = new Font("Segoe UI Symbol", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btn_Login.ForeColor = Color.White;
+            btn_Login.Location = new Point(289, 350);
+            btn_Login.Name = "btn_Login";
+            btn_Login.Size = new Size(143, 48);
+            btn_Login.TabIndex = 5;
+            btn_Login.Text = "LOGIN";
+            btn_Login.UseVisualStyleBackColor = false;
+            btn_Login.Click += btn_Login_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = Properties.Resources.bg;
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(731, 450);
+            Controls.Add(btn_Login);
+            Controls.Add(lblPass);
+            Controls.Add(txtUser);
+            Controls.Add(lblUser);
+            Controls.Add(lblTitle);
+            Controls.Add(txtPass);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblTitle;
+        private TextBox txtUser;
+        private TextBox txtPass;
+        private Label lblUser;
+        private Label lblPass;
+        private Button btn_Login;
+    }
+}
---
+++ Maturan/Form1.cs
@@ -0,0 +1,15 @@
+namespace Maturan
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Login_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
