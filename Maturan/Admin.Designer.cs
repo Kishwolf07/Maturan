516a1e9c6743d09822dcce7ef13153041bcbda7a
ACT-STUDENT <ACT-STUDENT@LAB4-PC24>
2025-07-28 11:53:32 +0800

    ang form gepa ninfdot

--- Maturan/Admin.Designer.cs
+++ Maturan/Admin.Designer.cs
@@ -30,46 +30,164 @@
         {
             lblAdmin = new Label();
             lbladminuser = new Label();
+            panel1 = new Panel();
+            button5 = new Button();
+            button4 = new Button();
+            button3 = new Button();
+            button2 = new Button();
+            btndashboard = new Button();
+            pictureBox1 = new PictureBox();
+            button6 = new Button();
+            button7 = new Button();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
             // lblAdmin
             // 
             lblAdmin.AutoSize = true;
-            lblAdmin.Font = new Font("Segoe UI", 24F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            lblAdmin.Location = new Point(204, 170);
+            lblAdmin.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblAdmin.Location = new Point(25, 82);
             lblAdmin.Name = "lblAdmin";
-            lblAdmin.Size = new Size(265, 45);
+            lblAdmin.Size = new Size(57, 15);
             lblAdmin.TabIndex = 0;
-            lblAdmin.Text = "Welcome Admin!";
+            lblAdmin.Text = "Welcome";
             // 
             // lbladminuser
             // 
             lbladminuser.AutoSize = true;
-            lbladminuser.Font = new Font("Segoe UI", 21.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            lbladminuser.Location = new Point(475, 174);
+            lbladminuser.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lbladminuser.Location = new Point(79, 82);
             lbladminuser.Name = "lbladminuser";
-            lbladminuser.Size = new Size(94, 40);
+            lbladminuser.Size = new Size(53, 15);
             lbladminuser.TabIndex = 1;
-            lbladminuser.Text = "label1";
+            lbladminuser.Text = "<name>";
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(button7);
+            panel1.Controls.Add(button6);
+            panel1.Controls.Add(button5);
+            panel1.Controls.Add(button4);
+            panel1.Controls.Add(button3);
+            panel1.Controls.Add(button2);
+            panel1.Controls.Add(btndashboard);
+            panel1.Controls.Add(pictureBox1);
+            panel1.Controls.Add(lbladminuser);
+            panel1.Controls.Add(lblAdmin);
+            panel1.Location = new Point(0, 1);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(151, 447);
+            panel1.TabIndex = 2;
+            // 
+            // button5
+            // 
+            button5.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button5.Location = new Point(25, 301);
+            button5.Name = "button5";
+            button5.Size = new Size(102, 37);
+            button5.TabIndex = 7;
+            button5.Text = "button5";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button4.Location = new Point(25, 258);
+            button4.Name = "button4";
+            button4.Size = new Size(102, 37);
+            button4.TabIndex = 6;
+            button4.Text = "button4";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button3.Location = new Point(25, 215);
+            button3.Name = "button3";
+            button3.Size = new Size(102, 37);
+            button3.TabIndex = 5;
+            button3.Text = "button3";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button2.Location = new Point(25, 172);
+            button2.Name = "button2";
+            button2.Size = new Size(102, 37);
+            button2.TabIndex = 4;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // btndashboard
+            // 
+            btndashboard.Font = new Font("Showcard Gothic", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btndashboard.Location = new Point(25, 118);
+            btndashboard.Name = "btndashboard";
+            btndashboard.Size = new Size(102, 37);
+            btndashboard.TabIndex = 3;
+            btndashboard.Text = "Dashboard";
+            btndashboard.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.admin;
+            pictureBox1.Location = new Point(25, 15);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(102, 63);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 2;
+            pictureBox1.TabStop = false;
+            // 
+            // button6
+            // 
+            button6.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button6.Location = new Point(25, 344);
+            button6.Name = "button6";
+            button6.Size = new Size(102, 37);
+            button6.TabIndex = 8;
+            button6.Text = "button6";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            button7.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button7.Location = new Point(25, 400);
+            button7.Name = "button7";
+            button7.Size = new Size(102, 37);
+            button7.TabIndex = 9;
+            button7.Text = "button7";
+            button7.UseVisualStyleBackColor = true;
             // 
             // Admin
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Controls.Add(lbladminuser);
-            Controls.Add(lblAdmin);
+            Controls.Add(panel1);
             FormBorderStyle = FormBorderStyle.None;
             Name = "Admin";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Admin";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
 
         private Label lblAdmin;
         private Label lbladminuser;
+        private Panel panel1;
+        private Button btndashboard;
+        private PictureBox pictureBox1;
+        private Button button5;
+        private Button button4;
+        private Button button3;
+        private Button button2;
+        private Button button6;
+        private Button button7;
     }
 }
--- Maturan/Properties/Resources.Designer.cs
+++ Maturan/Properties/Resources.Designer.cs
@@ -63,6 +63,16 @@ namespace Maturan.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap admin {
+            get {
+                object obj = ResourceManager.GetObject("admin", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap bg {
             get {
                 object obj = ResourceManager.GetObject("bg", resourceCulture);
